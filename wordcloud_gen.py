dd0b03e18baae5144d1289f92e74849385f784a0
TusharAgarwal10 <tusharagarwal1016@gmail.com>
2024-04-18 16:55:37 +0530

    new update

--- wordcloud_gen.py
+++ wordcloud_gen.py
@@ -19,13 +19,13 @@ for page_num in range(len(pdf_read.pages)):
 # for .txt file type, convert the code block for pdf into comments and un-comming the following line of code
 #text = open("file name", "r", encoding="utf-8").read() 
 
-text = contractions.fix(text)
+text = contractions.fix(text) 
 
 # this line of code excludes certain words from the word cloud. Here, "STOPWORDS" removes filler words like "the," "an," "a," etc.
-fillers = STOPWORDS # .update([]) in case you want to exclude specific words #here
+fillers = STOPWORDS # .update([]) in case you want to exclude specific words
 
 wc = WordCloud(stopwords = fillers, height = 1000, width = 1500).generate(text) # add background_color="" as an argument in the Wordcloud() function
-# wc.recolor(colormap="") in case you want to change the color fo the 
+# wc.recolor(colormap="") in case you want to change the color of the wordcloud
 
 plt.imshow(wc)
 plt.axis("off")
